0dede6518bc8e7d9832c1b9d2915eeb32188ba8d
khawla_ben <refeser52@gmail.com>
2024-05-07 13:51:24 +0000

    check

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
-# Simply matching Holberton
-puts ARGV[0].scan(/School/).join
+
+def match_school(str)
+	regex = /School/
+	match = str.match(regex)
+	puts match ? match[0] : ""
+  end
+  
+  if ARGV.length != 1
+	puts "Usage: #{$PROGRAM_NAME} <string>"
+	exit 1
+  end
+  
+  match_school(ARGV[0])
