88585dd02805747953588a9dcea598eadd0def09
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-08-02 03:50:34 +0100

    push

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,24 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 <script_name> <employee_id>")
+        exit(1)
+    
     employee_id = argv[1]
     api = 'https://jsonplaceholder.typicode.com/'
 
-    # Fetch employee data
-    endpoint = 'users/{}'.format(employee_id)
+    endpoint = f'users/{employee_id}'
     employee = requests.get(api + endpoint).json()
-    
-    # Fetch tasks for the employee
-    endpoint = 'todos?userId={}'.format(employee_id)
+
+    endpoint = f'todos?userId={employee_id}'
     tasks = requests.get(api + endpoint).json()
 
-    # Extract employee name and task details
     employee_name = employee.get("name")
-    task_count = len(tasks)
+    total_tasks = len(tasks)
     completed_tasks = [task for task in tasks if task.get("completed")]
+    completed_count = len(completed_tasks)
 
-    # Print the results in the expected format
-    print("Employee Name: {}".format(employee_name))
-    print("To Do Count: {}".format(task_count))
-    print("Completed Tasks:")
-    for i, task in enumerate(completed_tasks, start=1):
-        print("Task {}: {}".format(i, task.get("title")))
+    print(f"Employee {employee_name} is done with tasks({completed_count}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
