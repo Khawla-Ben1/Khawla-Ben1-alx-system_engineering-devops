37ff970f2683e32cdaa8d1c0f9d78a0317e2d33b
Khawla Ben hammou <kben-ham@e3r6p4.1337.ma>
2024-08-15 02:28:12 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,7 @@
 # This Puppet manifest fixes the file permissions for the Apache web root directory
 # to resolve a 500 Internal Server Error caused by permission issues.
 
-# Ensure that the directory /var/www/html exists and has the correct permissions.
-file { '/var/www/html':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
-  recurse => true,
-}
-
-# Restart the Apache service to apply changes
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/var/www/html'],
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
