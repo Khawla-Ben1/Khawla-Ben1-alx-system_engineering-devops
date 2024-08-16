9286c2a705a8eea985b6b5ac3325ebe690be2a7e
Khawla Ben hammou <kben-ham@e2r2p2.1337.ma>
2024-08-16 23:49:32 +0100

    update  x16

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,21 +7,23 @@ import requests
 
 
 def top_ten(subreddit):
-	"""
-	Queries the Reddit API and prints the titles of the first 10 hot posts
-	listed for a given subreddit. If the subreddit is invalid, prints None.
-	"""
-	url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-	headers = {'User-Agent': 'my-user-agent'}
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit. If the subreddit is invalid, prints None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'my-user-agent'}
 
-	try:
-		response = requests.get(url, headers=headers, allow_redirects=False)
-		if response.status_code == 200:
-			data = response.json()
-			posts = data['data']['children']
-			for post in posts:
-				print(post['data']['title'])
-		else:
-			print(None)
-	except Exception:
-		print(None)
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.exceptions.JSONDecodeError:
+        print(None)
+    except requests.exceptions.RequestException:
+        print(None)
