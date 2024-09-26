162fa2fadb5ef99e44bdca60e577bfc643d3aba2
Khawla Ben hammou <kben-ham@e1r5p2.1337.ma>
2024-09-26 11:57:54 +0100

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-
-""" Export api to csv"""
-
+"""Gather data from an API"""
 import csv
 import requests
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    user = sys.argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-    res = requests.get(url_user)
-    user_name = res.json().get('username')
-    task = url_user + '/todos'
-    res = requests.get(task)
-    tasks = res.json()
 
-    with open('{}.csv'.format(user), 'w') as csvfile:
-        for task in tasks:
-            completed = task.get('completed')
-            """Completed"""
-            title_task = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, user_name, completed, title_task))
+def gatherData(employeeID):
+    """Gather data from an API and save it to a CSV file"""
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}/todos"
+
+    try:
+        response = requests.get(user_url)
+        if response.status_code == 200:
+            user = response.json()
+            response = requests.get(todo_url)
+            if response.status_code == 200:
+                todos = response.json()
+                with open('{}.csv'.format(employeeID), 'w') as csvfile:
+                    writer = csv.writer(csvfile, delimiter=',',
+                                        quotechar='"', quoting=csv.QUOTE_ALL)
+                    for todo in todos:
+                        writer.writerow([todo['userId'], user['username'],
+                                        todo['completed'], todo['title']])
+            else:
+                print("An error occured")
+        else:
+            print("An error occured")
+    except (Exception):
+        print("An error occured")
+        return 0
+
+
+if __name__ == "__main__":
+    """ Only executes as main"""
+    if len(argv) == 2 and argv[1].isdigit():
+        gatherData(argv[1])
+    else:
+        print("Usage: ./1-export_to_CSV.py <employee ID>")
