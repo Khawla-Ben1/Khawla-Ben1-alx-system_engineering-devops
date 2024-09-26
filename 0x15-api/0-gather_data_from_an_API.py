3e088f6c8e29b93f6183abe96f8951102beed1fb
Khawla Ben hammou <kben-ham@e1r5p2.1337.ma>
2024-09-26 11:56:37 +0100

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python3
-'''
-Gather employee data from API and display TODO list progress.
-'''
+"""Gather data from an API"""
 import requests
-import sys
+from sys import argv
+
+
+def gatherData(employeeID):
+    """Gather data from an API and print it"""
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}/todos"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
+
+    try:
+        response = requests.get(user_url)
+        if response.status_code == 200:
+            user = response.json()
+            response = requests.get(todo_url)
+            if response.status_code == 200:
+                todos = response.json()
+                completed = []
+                for todo in todos:
+                    if todo['completed'] is True:
+                        completed.append(todo)
+                print("Employee {} is done with tasks({}/{}):"
+                      .format(user['name'], len(completed), len(todos)))
+                for todo in completed:
+                    print("\t {}".format(todo['title']))
+            else:
+                print("An error occured")
+        else:
+            print("An error occured")
+    except (Exception):
+        print("An error occured")
+        return 0
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    """ Only executes as main"""
+    if len(argv) == 2 and argv[1].isdigit():
+        gatherData(argv[1])
+    else:
+        print("Usage: ./0-gather_data_from_an_API.py <employee ID>")
