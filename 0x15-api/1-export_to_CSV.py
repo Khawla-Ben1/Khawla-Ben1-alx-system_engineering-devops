47844077d0c8174f4e78baa908674e57c44b7fe4
Khawla Ben hammou <kben-ham@e1r5p2.1337.ma>
2024-09-26 11:59:19 +0100

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+"""
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+and export data in the CSV format.
+"""
+
 import csv
+import json
 import requests
 from sys import argv
 
 
-def gatherData(employeeID):
-    """Gather data from an API and save it to a CSV file"""
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}/todos"
-
-    try:
-        response = requests.get(user_url)
-        if response.status_code == 200:
-            user = response.json()
-            response = requests.get(todo_url)
-            if response.status_code == 200:
-                todos = response.json()
-                with open('{}.csv'.format(employeeID), 'w') as csvfile:
-                    writer = csv.writer(csvfile, delimiter=',',
-                                        quotechar='"', quoting=csv.QUOTE_ALL)
-                    for todo in todos:
-                        writer.writerow([todo['userId'], user['username'],
-                                        todo['completed'], todo['title']])
-            else:
-                print("An error occured")
-        else:
-            print("An error occured")
-    except (Exception):
-        print("An error occured")
-        return 0
-
-
 if __name__ == "__main__":
-    """ Only executes as main"""
-    if len(argv) == 2 and argv[1].isdigit():
-        gatherData(argv[1])
-    else:
-        print("Usage: ./1-export_to_CSV.py <employee ID>")
+
+    sessionReq = requests.Session()
+
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    usr = employeeName.json()['username']
+
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    fileCSV = idEmp + '.csv'
+
+    with open(fileCSV, "w", newline='') as csvfile:
+        write = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+        for i in json_req:
+            write.writerow([idEmp, usr, i.get('completed'), i.get('title')])
