f08fa1d87c8318bc0bf6c7e568f6fd52a1274b77
Khawla Ben hammou <kben-ham@e1r5p2.1337.ma>
2024-09-26 12:24:55 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,8 +13,7 @@ def count_words(subreddit, word_list, hot_list=None, after=None):
     Recursively queries the Reddit API to count occurrences of
     keywords in hot article titles
     for a given subreddit. Results are printed in descending
-    order of frequency and alphabetically
-    if frequencies are the same.
+    order of frequency and alphabetically if frequencies are the same.
 
     Args:
         subreddit (str): The subreddit to query.
@@ -33,7 +32,9 @@ def count_words(subreddit, word_list, hot_list=None, after=None):
     params = {'after': after}
 
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+        )
         if response.status_code != 200:
             return
 
@@ -50,13 +51,18 @@ def count_words(subreddit, word_list, hot_list=None, after=None):
             count_words(subreddit, word_list, hot_list, after)
         else:
             word_count = defaultdict(int)
-            word_pattern = re.compile(r'\b(?:' + '|'.join(map(re.escape, word_list)) + r')\b', re.IGNORECASE)
+            word_pattern = re.compile(
+                r'\b(?:' + '|'.join(map(re.escape, word_list)) + r')\b',
+                re.IGNORECASE
+            )
             for title in hot_list:
                 matches = word_pattern.findall(title)
                 for match in matches:
                     word_count[match.lower()] += 1
 
-            sorted_counts = sorted(word_count.items(), key=lambda item: (-item[1], item[0]))
+            sorted_counts = sorted(
+                word_count.items(), key=lambda item: (-item[1], item[0])
+            )
             for word, count in sorted_counts:
                 if word in word_list:
                     print(f"{word}: {count}")
