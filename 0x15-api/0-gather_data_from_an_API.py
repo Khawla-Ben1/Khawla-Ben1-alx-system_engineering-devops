285238702ee5b754437024fcd25e724f0d8dc176
khawla_ben <refeser52@gmail.com>
2024-09-27 17:05:22 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+""" With request ask for header"""
 import requests
-from sys import argv
-
-
-def gatherData(employeeID):
-    """Gather data from an API and print it"""
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}/todos"
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
-
-    try:
-        response = requests.get(user_url)
-        if response.status_code == 200:
-            user = response.json()
-            response = requests.get(todo_url)
-            if response.status_code == 200:
-                todos = response.json()
-                completed = []
-                for todo in todos:
-                    if todo['completed'] is True:
-                        completed.append(todo)
-                print("Employee {} is done with tasks({}/{}):"
-                      .format(user['name'], len(completed), len(todos)))
-                for todo in completed:
-                    print("\t {}".format(todo['title']))
-            else:
-                print("An error occured")
-        else:
-            print("An error occured")
-    except (Exception):
-        print("An error occured")
-        return 0
-
+import sys
 
 if __name__ == "__main__":
-    """ Only executes as main"""
-    if len(argv) == 2 and argv[1].isdigit():
-        gatherData(argv[1])
+    if len(sys.argv) != 2:
+        sys.exit(1)
     else:
-        print("Usage: ./0-gather_data_from_an_API.py <employee ID>")
+        my_id = sys.argv[1]
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(my_id)
+        dict1 = requests.get(url).json()
+        name = dict1.get('name')
+        url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.\
+               format(my_id)
+        list2 = requests.get(url2).json()
+        list_tasks = []
+        t_total = 0
+        t_done = 0
+        for tasks in list2:
+            if tasks.get('userId') == int(my_id):
+                t_total += 1
+                if tasks.get('completed'):
+                    list_tasks.append(tasks.get('title'))
+                    t_done += 1
+        print("Employee {} is done with tasks({}/{}):".format(name,
+                                                              t_done, t_total))
+        for tasks in list_tasks:
+            print("\t", tasks)
